615a3415e049375f2a6a7131e055b2985951cc58
Irene <irenehuang189@gmail.com>
2016-04-13 02:21:26 +0700

    Create environment

---
+++ EndlessArcadeGame/Assets/Scripts/GroundControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundControl : MonoBehaviour {
+    // Attribute
+    float speed = .5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Time.time * speed;
+        renderer.material.mainTextureOffset = new Vector2(0, -offset);
+	}
+}
